975619571b075e02002cc0c616ed00e20d3432c2
HugoLipeng <1924949262@qq.com>
2018-09-08 07:55:05 +0800

    init the code

---
+++ basic/do_excise_hugo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+
+print("a is {}, b is {}".format("a","b"))
+
+string = 'hugo'
+
+print(len(string))
+print(id(string))
+
+# 空列表
+variable = []
+variable = list()
