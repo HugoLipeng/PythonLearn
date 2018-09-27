4ec19660e35555511cfb198883e0ab0e90768884
HugoLipeng <1924949262@qq.com>
2018-09-27 22:09:31 +0800

    add muke_jiuyue_code

---
+++ muke_jiuyue_code/进制转换.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+# 其他进制转换2进制
+print(bin(0xE))
+
+# 其他进制转换成10进制
+print(int(0x888))
+
+# 其他进制转换成8进制
+print(oct(888))
+
+# 其他进制转换成16进制
+print(hex(888))
